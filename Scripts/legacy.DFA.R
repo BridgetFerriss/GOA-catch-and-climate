c119ef2b73ae781f4c160000b2daa6549af6d0c3
mikelitzow <mike.litzow@noaa.gov>
2022-01-27 08:03:20 -0900

    First round of model selection for legacy catch DFA

--- Scripts/legacy.DFA.R
+++ Scripts/legacy.DFA.R
@@ -66,4 +66,70 @@ change <- is.na(dfa.dat)
 dfa.dat[change] <- 0
 
 # divide by 1000s of t and fourth-root transform
-dfa.dat <- log()
+dfa.dat <- (dfa.dat/1000)^0.25
+
+# make some new exploratory plots
+
+dfa.dat <- dfa.dat %>%
+  mutate(year = 1956:1990) %>%
+  pivot_longer(cols = -year)
+
+# first look at time series
+ggplot(dfa.dat, aes(year, value)) +
+  geom_line() +
+  geom_point() +
+  facet_wrap(~name)
+
+# now distributions
+ggplot(dfa.dat, aes(value)) +
+  geom_histogram(bins = 12, fill = "grey", color = "black") +
+  facet_wrap(~name, scales = "free")
+
+# fit a DFA model ---------------------------------------------
+# (commenting this out, uncomment to run model selection)
+# set up data
+
+dfa.dat <- dfa.dat %>%
+  pivot_wider(names_from = name, values_from = value) %>% # this gives 1956:1990; use all
+  select(-year) %>%
+  t()
+  
+
+colnames(dfa.dat) <- 1956:1990
+
+# set up forms of R matrices
+levels.R = c("diagonal and equal",
+             "diagonal and unequal",
+             "equalvarcov",
+             "unconstrained")
+model.data = data.frame()
+
+# changing convergence criterion to ensure convergence
+cntl.list = list(minit=200, maxit=20000, allow.degen=FALSE, conv.test.slope.tol=0.1, abstol=0.0001)
+
+# fit models & store results
+for(R in levels.R) {
+  for(m in 1:2) {  # allowing up to 2 trends
+    dfa.model = list(A="zero", R=R, m=m)
+    kemz = MARSS(dfa.dat, model=dfa.model,
+                 form="dfa", z.score=TRUE, control=cntl.list)
+    model.data = rbind(model.data,
+                       data.frame(R=R,
+                                  m=m,
+                                  logLik=kemz$logLik,
+                                  K=kemz$num.params,
+                                  AICc=kemz$AICc,
+                                  stringsAsFactors=FALSE))
+    assign(paste("kemz", m, R, sep="."), kemz)
+  } # end m loop
+} # end R loop
+
+# calculate delta-AICc scores, sort in descending order, and compare
+model.data$dAICc <- model.data$AICc-min(model.data$AICc)
+model.data <- model.data %>%
+  arrange(dAICc)
+model.data
+
+# save model selection table
+write.csv(model.data, "./Results/legacy catch dfa model selection table 1956-1990.csv",
+          row.names = F)
